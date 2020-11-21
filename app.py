04cd964e3938d80d6e996f2d7078bb8aaee9d0ca
bridcourtney <brid.courtney@gmail.com>
2020-11-21 17:34:03 +0000

    Create add_check template

--- app.py
+++ app.py
@@ -95,6 +95,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_check")
+def add_check():
+    return render_template("add_check.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
