f33034c7b624e26964e42a8f96f41214a9a17354
bridcourtney <brid.courtney@gmail.com>
2020-12-06 17:19:21 +0000

    Added Registration Form Validation with WTForms, replaced check_password_hash in app.py with validation in forms.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from flask_pymongo import PyMongo
 from datetime import datetime
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
-from myforms.forms import Add_CheckForm
+from myforms.forms import RegistrationForm, Add_CheckForm
 if os.path.exists("env.py"):
     import env
-
+    
 
 app = Flask(__name__)
 
@@ -37,35 +37,29 @@ def search():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST":
+    form = RegistrationForm()
+    if form.validate() and request.method == "POST":
         # check if username already exists in db
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+            {"username": request.form.get("username")})
 
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
 
-        # confirm password using werkzeug.security.check_password_hash(pwhash, password)
-        if check_password_hash(generate_password_hash
-        (request.form.get("password")),
-        request.form.get("cpassword")):
-            flash("Registration Successful!")
-        else:
-            flash("passwords don't match, please try again")
-            return redirect(url_for("register"))
-
         register = {
-            "fname": request.form.get("fname").lower(),
-            "username": request.form.get("username").lower(),
+            "fname": request.form.get("fname"),
+            "username": request.form.get("username"),
             "password": generate_password_hash(request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
         return redirect(url_for("profile", username=session["user"]))
-    return render_template("register.html")
+    return render_template("register.html", form=form)
+
 
 
 @app.route("/login", methods=["GET", "POST"])
--- myforms/forms.py
+++ myforms/forms.py
@@ -1,7 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField,\
-     TextAreaField, IntegerField
-from wtforms.validators import DataRequired, Length, EqualTo, Optional
+from wtforms import Form, BooleanField, StringField,\
+validators, PasswordField, SubmitField
+from wtforms.validators import DataRequired, length, EqualTo, Optional
+
+class RegistrationForm(FlaskForm):
+    fname = StringField('Firstname', [validators.length(min=3, max=15)])
+    username = StringField('Username', [validators.length(min=3, max=15)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Repeat Password')
+    submit = SubmitField('Register')
 
 class Add_CheckForm(FlaskForm):
 
