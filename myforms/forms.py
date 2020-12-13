acfd77108ee9a5dc2a09d35ed371f85e3027781a
bridcourtney <brid.courtney@gmail.com>
2020-12-13 00:38:17 +0000

    Added Change Password form, started wtForm for Change Password form

--- myforms/forms.py
+++ myforms/forms.py
@@ -3,6 +3,7 @@ from wtforms import Form, BooleanField, StringField,\
 validators, PasswordField, SubmitField
 from wtforms.validators import DataRequired, length, EqualTo, Optional
 
+
 class RegistrationForm(FlaskForm):
     fname = StringField('Firstname', [validators.length(min=2, max=15)])
     username = StringField('Username', [validators.length(min=3, max=15)])
@@ -13,10 +14,19 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
 
+class ChangePassForm(FlaskForm):
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Repeat Password')
+    submit = SubmitField('Change')
+
+
 class LoginForm(FlaskForm):
     username = StringField('Username', [validators.length(min=3, max=15)])
     password = PasswordField('Password', [validators.DataRequired()])
     submit = SubmitField('Login')
+
 
 class Add_CheckForm(FlaskForm):
     image = StringField('Workstation Image', validators=[Optional()])
